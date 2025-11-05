0a0c4d3ed0847038d0bdb16de0adb11dd8c51671
Likitha <likithakrishnappaa@gmail.com>
2025-11-05 14:54:01 +0530

    Implement multiple utility functions in Day-3.js
    Added functions to find the longest word, square a number, greet a user, and repeat a string.

--- Day-3.js
+++ Day-3.js
@@ -1,1 +1,42 @@
+/-------find the longest word in js--------
+function longestWordInSentence(sentence) {
+let longest = "";
+let current = "";
+for(let i = 0; i <= sentence.length; i++) {
+  let char = sentence[i];
+ 
+if(char === '' || i === sentence.length) {
+  if(current.length > longest.length){
+    longest = current;
+  }
+  current ='';
+} else {
+  current += char;
+}
+}
+return longest;
+}
+let sentence = "Click here to run the Javascript";
+let result = longestWordInSentence(sentence);
+console.log("The longest word is", result);
+
+//---arrow functions-----
+const square = x => x * x;
+console.log(square(2));
+
+// const greet = name => {
+  const message = "Hello" + name;
+  return message;
+ };
+  console.log(greet("Manny"));
+
+//------repeat a string N times---
+const repeatString = (str, n) => {
+  let result = "";
+  for( let i = 0; i < n ; i++) {
+    result += str;
+  }
+  return result;
+ };
+ console.log(repeatString("Pyari " , 4));
 
